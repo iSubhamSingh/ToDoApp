b36a2b3bcada3f318a4db2ef0b7414d432eb5722
Subham <subhamsingh5638@gmail.com>
2024-04-01 17:26:46 +0530

    added additional functionality

--- frontend/script.js
+++ frontend/script.js
@@ -1,40 +1,55 @@
-
-
-function loadFn(){
-    fetch("http://localhost:3000/todos",{
-        method: "GET"
-    }).then((resp) => {
-        return resp.json();
-    }).then((data) => {
-        let div = document.getElementById("todoItems");
-        for(let i = 0; i <data.length; i++){
-            
-            let item =  div.appendChild(document.createElement("item"));
-            item.innerHTML = `<div style = "border:2px solid white; margin: 4px;"><p><strong>${data[i].title}</strong></p>  
+function loadFn() {
+  fetch("http://localhost:3000/todos", {
+    method: "GET",
+  })
+    .then((resp) => {
+      return resp.json();
+    })
+    .then((data) => {
+      let div = document.getElementById("todoItems");
+      for (let i = 0; i < data.length; i++) {
+        let item = div.appendChild(document.createElement("item"));
+        item.innerHTML = `<div id=${data[i].id} style = " margin-top: 34px;" ">
+                            <p><strong>${data[i].title}</strong> 
+                                <button id=${data[i].id} style="margin-left:10px" class="btn" onClick="deleteFn(this.id)"><i class="fa fa-trash"></i></button> </p>  
                               <p>${data[i].description}</p></div>`;
-        }
+      }
+    });
+}
+loadFn();
+
+function submitFn() {
+  let newTitle = document.getElementById("title").value;
+  let newDesc = document.getElementById("desc").value;
+  if (newTitle.trim().length === 0 || newDesc.trim().length === 0) {
+    alert("Please enter valid inputs");
+    return;
+  }
+  fetch("http://localhost:3000/todos", {
+    method: "POST",
+    body: JSON.stringify({
+      title: newTitle,
+      description: newDesc,
+    }),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  })
+    .then((resp) => {
+      return resp.json();
     })
+    .then((data) => {
+      console.log(document.getElementById("title").value);
+    });
 }
 
-
-
-function submitFn(){
-    
-    fetch("http://localhost:3000/todos", {
-        method: "POST",
-        body : JSON.stringify({
-            title: document.getElementById("title").innerText,
-            description: document.getElementById("desc").innerText
-        }),
-        headers: {
-            "Content-Type": "application/json"
-        }
-    }).then((resp) => {
-        return resp.json();
-    }).then((data) => {
-        console.log(document.getElementById("title").innerText);
-        
+function deleteFn(id) {
+  fetch(`http://localhost:3000/todos/${id}`, { method: "DELETE" })
+    .then((resp) => {
+      return resp.json();
     })
+    .then((data) => {
+      console.log(data);
+    });
+  console.log(id);
 }
-
-
