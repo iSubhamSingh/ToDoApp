1e9559df13ec03005357f43b392d8fa9239624df
Subham <subhamsingh5638@gmail.com>
2024-08-26 23:15:53 +0530

    added random id

--- server.js
+++ server.js
@@ -42,7 +42,7 @@ app.get("/todos/:id", (req, res) => {
 app.post("/todos", (req, res) => {
    
   let newTodo = {
-    id : Math.floor(Math.random()*10000)+1,
+    id : Date.now(),
     title : req.body.title,
     description : req.body.description
   };
